1747725bf5d7ace8c09f86403bab368e1a3131b1
MonkeyDRemi <remi.vavichandran2@gmail.com>
2025-01-11 20:29:09 +0100

    Boucle pour la creation de table main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,33 @@
 #include "database.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     printf("Bienvenue dans le gestionnaire de base de données.\n");
-    Table *table = create_table("default_table");
+    
+    Table *table = NULL;
+    char command[256];
 
-    // Affichage pour vérifier la création de la table
-    printf("Table créée : %s\n", table->name);
+    while (1) {
+        printf("db> ");
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = 0;
 
-    free_table(table); // Libération des ressources
+        if (strncmp(command, "CREATE", 6) == 0) {
+            char name[256];
+            sscanf(command + 7, "%s", name);
+            if (table) free_table(table);
+            table = create_table(name);
+            printf("Table '%s' créée.\n", name);
+        } else if (strncmp(command, "EXIT", 4) == 0) {
+            printf("Quitter...\n");
+            break;
+        } else {
+            printf("Commande inconnue! Utilisez CREATE ou EXIT.\n");
+        }
+    }
+
+    if (table) free_table(table);
     return 0;
 }
